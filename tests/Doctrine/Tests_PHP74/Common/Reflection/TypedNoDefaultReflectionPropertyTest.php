70707057f60f9081453ed305bec29fcbdd81d6ee
Patrik Foldes <studio22@mail.ru>
2019-12-02 17:16:00 +0300

    Added reflection class for handling typed properties without default value #23

---
+++ tests/Doctrine/Tests_PHP74/Common/Reflection/TypedNoDefaultReflectionPropertyTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Doctrine\Tests_PHP74\Common\Reflection;
+
+use Doctrine\Common\Reflection\TypedNoDefaultReflectionProperty;
+use PHPUnit\Framework\TestCase;
+
+class TypedNoDefaultReflectionPropertyTest extends TestCase
+{
+    public function testGetValue() : void
+    {
+        $object = new TypedNoDefaultReflectionPropertyTestClass();
+
+        $reflProperty = new TypedNoDefaultReflectionProperty(TypedNoDefaultReflectionPropertyTestClass::class, 'test');
+
+        self::assertSame(null, $reflProperty->getValue($object));
+
+        $object->test = 'testValue';
+
+        self::assertSame('testValue', $reflProperty->getValue($object));
+
+        unset($object->test);
+
+        self::assertNull($reflProperty->getValue($object));
+    }
+}
+
+class TypedNoDefaultReflectionPropertyTestClass
+{
+    public string $test;
+}
